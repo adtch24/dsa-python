9914e33c589109d8cf68057eb676ba382a795c66
Aditya Choudhary <adityachoudhary2602@gmail.com>
2026-07-04 12:31:03 +0530

    Add binary search example - O(log n) complexity demo

---
+++ 01_complexity_analysis/binary_search.py
@@ -0,0 +1,20 @@
+# Example used to demonstrate O(log n) time complexity
+def find_name(names,target):
+    left=0
+    right=len(names)-1
+    while(left<=right):
+        middle=(left+right)//2
+        if(names[middle]==target):
+           return middle
+        elif(names[middle]<=target):
+           left = middle-1
+        else:
+            right=middle+1
+    return -1
+names=["Aditya","Bunny","Cat","David","Enna"]   # Should be in alphabetical order
+target="Cat"
+result=find_name(names,target)
+if(result==-1):
+ print("Not found")
+else:
+   print("Found")
